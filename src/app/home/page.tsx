567cce2cd0d0eb723d84cbdc0d58d37fcbea6662
najmiter <realnajmiter@gmail.com>
2024-06-05 01:54:06 +0500

    ok

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -10,18 +10,28 @@ export default async function page() {
             <main className="max-w-[80rem] space-y-10 mx-auto p-10 rounded-lg h-full shadow-md">
                 <div className="space-y-2">
                     <h1 className="text-3xl font-bold uppercase">Your files</h1>
-                    <p className="text-sm">Here are all your files</p>
+                    {!!data?.length && (
+                        <p className="text-md">
+                            Select any of your files to start editing
+                        </p>
+                    )}
                 </div>
-                <div className="flex flex-col gap-3">
-                    {data?.map((doc, i) => (
-                        <div
-                            key={i}
-                            className="cursor-pointer hover:bg-stone-200"
-                        >
-                            <DocumentCard document={doc} />
-                        </div>
-                    ))}
-                </div>
+                {data?.length ? (
+                    <div className="flex flex-col gap-3">
+                        {data?.map((doc, i) => (
+                            <div
+                                key={i}
+                                className="cursor-pointer hover:bg-stone-200"
+                            >
+                                <DocumentCard document={doc} />
+                            </div>
+                        ))}
+                    </div>
+                ) : (
+                    <div className="flex items-center justify-center">
+                        You don&apos;t have any files saved yet :)
+                    </div>
+                )}
             </main>
         </div>
     );
